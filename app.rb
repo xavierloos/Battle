b9f883ff407f7177137f3570f0677f6e8832af9b
Ara <hello.ara@pm.me>
2020-12-09 14:37:40 +0000

    adding sessions

--- app.rb
+++ app.rb
@@ -1,16 +1,23 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base # Base class for all Sinatra applications and middleware
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player_1 = params[:player_1]
-    @player_2 = params[:player_2]
-    erb :play
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+    redirect to('/play')
   end
 
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb :play
+  end
 
   # start the server if ruby file executed directly
   run! if app_file == $0
